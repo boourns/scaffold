e258f852607e988ac6b8a584db7611ac085ed92a
Tom Burns <tom.burns@shopify.com>
2015-04-29 14:22:13 +0000

    output filenames are now generated in current directory

--- model/scaffold.go
+++ model/scaffold.go
@@ -18,7 +18,7 @@ func (c model) Generate(m *ast.Model) (error) {
 	out := bytes.NewBuffer(nil)
 	err := modelTemplate(out, m)
 
-	outFileName := fmt.Sprintf("%s-model.go", strings.ToLower(m.Name))
+	outFileName := fmt.Sprintf("%s_sql.go", strings.ToLower(m.Name))
 
 	fmt.Printf("- Saving as %s\n", outFileName)
 	err = ioutil.WriteFile(outFileName, []byte(out.Bytes()), 0644)
