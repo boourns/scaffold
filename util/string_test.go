d9a440fd80edc8a5425a9ef8501e2a0a4fe123bb
Kevin Barnes <iamkevb@gmail.com>
2021-12-01 15:22:55 -0500

    TestTransform

--- util/string_test.go
+++ util/string_test.go
@@ -5,7 +5,20 @@ import "testing"
 func TestQuestionMarks(t *testing.T) {
 	expected := "?,?,?,?,?,?,?,?,?,?"
 	got := QuestionMarks(10)
-	if expected != QuestionMarks(3) {
+	if expected != got {
 		t.Errorf("Expected %s, but got %s", expected, got)
+	}
+}
+
+func TestTransform(t *testing.T) {
+	expected := []string{"<one>", "<two>", "<three>"}
+	got := Transform("<", []string{"one", "two", "three"}, ">")
+	if len(expected) != len(got) {
+		t.Errorf("Expected []string of with len %d, got len %d", len(expected), len(got))
+	}
+	for i, v := range expected {
+		if v != got[i] {
+			t.Errorf("Expected %s, but got %s", v, got[i])
+		}
 	}
 }
